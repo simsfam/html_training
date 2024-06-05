d057c7cb38dd5efc942f547f3c4875ed34254e46
ian <iansims7@gmail.com>
2024-06-05 14:49:10 -0500

    implement smooth scrolling on the links

--- 01-test/html-css-course-master/starter/07-Omnifood-Desktop/js/script.js
+++ 01-test/html-css-course-master/starter/07-Omnifood-Desktop/js/script.js
@@ -29,6 +29,35 @@ btnNavEl.addEventListener("click", function () {
 });
 
 ///////////////////////////////////////////////////////////
+// Smooth scrolling animation
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelectorAll(href);
+      sectionEl.scrollHeight({ behavior: "smooth" });
+    }
+
+    // Close mobile navigation
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
+
+///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
   var flex = document.createElement("div");
